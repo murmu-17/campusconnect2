03c3302488fe21c9d5012eb9c73218488d564233
murmu-17 <rajesh17murmu@gmail.com>
2026-03-25 12:00:10 +0530

    Fix sql_mode to disable ONLY_FULL_GROUP_BY

--- Campus-Connect/Backend/src/db/connection.js
+++ Campus-Connect/Backend/src/db/connection.js
@@ -6,6 +6,14 @@ const db = mysql.createConnection({
   user: DB_USER,
   password: DB_PASS,
   database: DB_NAME,
+  multipleStatements: false,
+});
+
+db.connect(function(err) {
+  if (err) { console.error("MySQL connection error:", err); return; }
+  db.query("SET SESSION sql_mode = 'STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION'", function(err) {
+    if (err) console.error("sql_mode error:", err);
+  });
 });
 
 module.exports = db;
